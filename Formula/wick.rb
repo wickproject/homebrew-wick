4286a52aee22b1312d92862cdc72f02d4560bbc3
Adam Fisk <afisk@getlantern.org>
2026-04-22 10:50:09 -0600

    wick 0.10.0

--- Formula/wick.rb
+++ Formula/wick.rb
@@ -1,15 +1,15 @@
 class Wick < Formula
   desc "Browser-grade web access for AI agents"
   homepage "https://getwick.dev"
-  version "0.9.1"
+  version "0.10.0"
   license "MIT"
 
   depends_on "yt-dlp" => :recommended
 
   on_macos do
     on_arm do
-      url "https://github.com/wickproject/wick/releases/download/v0.9.1/wick-darwin-arm64.tar.gz"
-      sha256 "7618186ea87d3eb08b96a56ef8c5fda04d0b0fafe634b09715ac652d1f8c2e30"
+      url "https://github.com/wickproject/wick/releases/download/v0.10.0/wick-darwin-arm64.tar.gz"
+      sha256 "5f8bc1dfd5897f31c526efe9d4898497f3bce6dacdb729764de35ca6bdba3f30"
     end
   end
 
